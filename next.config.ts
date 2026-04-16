8327df4ea46ef8e61a398a88cc44174414624492
Aurora Academy <aurora@academy.com>
2026-04-16 11:21:56 -0300

    fix: allow Meta Pixel domains in Content-Security-Policy

--- next.config.ts
+++ next.config.ts
@@ -92,7 +92,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.youtube.com https://s.ytimg.com; style-src 'self' 'unsafe-inline'; img-src * data:; font-src 'self' data:; connect-src *; frame-src 'self' https://www.youtube.com https://youtube.com https://www.youtube-nocookie.com; media-src 'self' https://*.ufs.sh https://utfs.io blob:;"
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.youtube.com https://s.ytimg.com https://connect.facebook.net https://www.facebook.com; style-src 'self' 'unsafe-inline'; img-src * data:; font-src 'self' data:; connect-src *; frame-src 'self' https://www.youtube.com https://youtube.com https://www.youtube-nocookie.com; media-src 'self' https://*.ufs.sh https://utfs.io blob:;"
           }
         ]
       }
